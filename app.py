7abc17970e6aea2e3f3a9501d8e842a27ed4ebaf
andy-uiw <58962659+andy-uiw@users.noreply.github.com>
2019-12-30 11:40:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,10 +68,22 @@ def handle_message(event):
             _message = TextSendMessage(text=cls)	#reply course
             line_bot_api.reply_message(event.reply_token, _message)
 #            line_bot_api.push_message(event.source.user_id, TextSendMessage(text='123'))
-    
+    elif '空氣' in _token[0] or 'pm2' in _low_token:
+        # query PM2.5
+        for _site in pm_site:
+            if _site == _token[1]:
+                _message = TextSendMessage(text=pm_site[_site]) #reply pm2.5 for the site
+                line_bot_api.reply_message(event.reply_token, _message)
+                break;
     elif '!h' in _token[0] or '!help' in _token[0]:
         _message = TextSendMessage(text="請輸入:課程, 誠品, 空氣 + <關鍵字>")
         line_bot_api.reply_message(event.reply_token, _message)
+	
+def loadPMJson():
+    with urllib.request.urlopen("http://opendata2.epa.gov.tw/AQI.json") as url:
+        data = json.loads(url.read().decode())
+        for ele in data:
+            pm_site[ele['SiteName']] = ele['PM2.5']
 
 def getCls(cls_prefix):
     ret_cls = []
@@ -96,7 +108,9 @@ def getCls(cls_prefix):
         
             
 import os
-
+if __name__ == "__main__":
+    # load PM2.5 records
+    loadPMJson()
     
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
